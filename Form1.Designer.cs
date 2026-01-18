b2856d7252a194e9b3617d6ed05a96f08735a4bb
BuildTools <unconfigured@null.spigotmc.org>
2026-01-18 21:12:40 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace MKLinkHelper_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SelectFolders_bt = new Button();
+            SelectFiles_fn = new OpenFileDialog();
+            SelectFolders_fn = new FolderBrowserDialog();
+            InputFolders = new TextBox();
+            SelectFiles_bt = new Button();
+            InputFiles = new TextBox();
+            LOG_label = new Label();
+            LogText = new TextBox();
+            SelectOutputs_bt = new Button();
+            OutputFolders = new TextBox();
+            CreateFoldersSymlinks = new Button();
+            CreateFolderJucntions = new Button();
+            CreateFileSymlinks = new Button();
+            CreateFileHardlinks = new Button();
+            OutputLogToText = new Button();
+            SuspendLayout();
+            // 
+            // SelectFolders_bt
+            // 
+            SelectFolders_bt.Location = new Point(12, 12);
+            SelectFolders_bt.Name = "SelectFolders_bt";
+            SelectFolders_bt.Size = new Size(94, 24);
+            SelectFolders_bt.TabIndex = 0;
+            SelectFolders_bt.Text = "Select Folders";
+            SelectFolders_bt.UseVisualStyleBackColor = true;
+            SelectFolders_bt.Click += SelectFolders_bt_Click;
+            // 
+            // InputFolders
+            // 
+            InputFolders.AllowDrop = true;
+            InputFolders.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            InputFolders.Location = new Point(112, 14);
+            InputFolders.Name = "InputFolders";
+            InputFolders.PlaceholderText = "Select Folders you want make Symlinks or Junctions of. You can drag and drop here";
+            InputFolders.Size = new Size(580, 23);
+            InputFolders.TabIndex = 3;
+            // 
+            // SelectFiles_bt
+            // 
+            SelectFiles_bt.Location = new Point(12, 41);
+            SelectFiles_bt.Name = "SelectFiles_bt";
+            SelectFiles_bt.Size = new Size(94, 23);
+            SelectFiles_bt.TabIndex = 4;
+            SelectFiles_bt.Text = "Select Files";
+            SelectFiles_bt.UseVisualStyleBackColor = true;
+            SelectFiles_bt.Click += SelectFiles_bt_Click;
+            // 
+            // InputFiles
+            // 
+            InputFiles.AllowDrop = true;
+            InputFiles.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            InputFiles.Location = new Point(112, 42);
+            InputFiles.Name = "InputFiles";
+            InputFiles.PlaceholderText = "Select Files you want make Symlinks or Hardlinks of. You can drag and drop here";
+            InputFiles.Size = new Size(580, 23);
+            InputFiles.TabIndex = 5;
+            // 
+            // LOG_label
+            // 
+            LOG_label.AutoSize = true;
+            LOG_label.BorderStyle = BorderStyle.FixedSingle;
+            LOG_label.Location = new Point(13, 70);
+            LOG_label.Name = "LOG_label";
+            LOG_label.Size = new Size(32, 17);
+            LOG_label.TabIndex = 6;
+            LOG_label.Text = "LOG";
+            // 
+            // LogText
+            // 
+            LogText.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            LogText.Location = new Point(12, 90);
+            LogText.Multiline = true;
+            LogText.Name = "LogText";
+            LogText.ReadOnly = true;
+            LogText.Size = new Size(680, 284);
+            LogText.TabIndex = 7;
+            LogText.WordWrap = false;
+            // 
+            // SelectOutputs_bt
+            // 
+            SelectOutputs_bt.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            SelectOutputs_bt.Location = new Point(13, 380);
+            SelectOutputs_bt.Name = "SelectOutputs_bt";
+            SelectOutputs_bt.Size = new Size(94, 23);
+            SelectOutputs_bt.TabIndex = 8;
+            SelectOutputs_bt.Text = "Select Outputs";
+            SelectOutputs_bt.UseVisualStyleBackColor = true;
+            // 
+            // OutputFolders
+            // 
+            OutputFolders.AllowDrop = true;
+            OutputFolders.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            OutputFolders.Location = new Point(113, 381);
+            OutputFolders.Name = "OutputFolders";
+            OutputFolders.PlaceholderText = "Select Folders you want output to. You can drag and drop here";
+            OutputFolders.Size = new Size(579, 23);
+            OutputFolders.TabIndex = 9;
+            // 
+            // CreateFoldersSymlinks
+            // 
+            CreateFoldersSymlinks.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CreateFoldersSymlinks.Location = new Point(12, 409);
+            CreateFoldersSymlinks.Name = "CreateFoldersSymlinks";
+            CreateFoldersSymlinks.Size = new Size(144, 23);
+            CreateFoldersSymlinks.TabIndex = 10;
+            CreateFoldersSymlinks.Text = "Create Folder Symlinks";
+            CreateFoldersSymlinks.UseVisualStyleBackColor = true;
+            CreateFoldersSymlinks.Click += CreateFoldersSymlinks_Click;
+            // 
+            // CreateFolderJucntions
+            // 
+            CreateFolderJucntions.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CreateFolderJucntions.Location = new Point(12, 438);
+            CreateFolderJucntions.Name = "CreateFolderJucntions";
+            CreateFolderJucntions.Size = new Size(144, 23);
+            CreateFolderJucntions.TabIndex = 11;
+            CreateFolderJucntions.Text = "Create Folder Junctions";
+            CreateFolderJucntions.UseVisualStyleBackColor = true;
+            CreateFolderJucntions.Click += CreateFolderJucntions_Click;
+            // 
+            // CreateFileSymlinks
+            // 
+            CreateFileSymlinks.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CreateFileSymlinks.Location = new Point(162, 410);
+            CreateFileSymlinks.Name = "CreateFileSymlinks";
+            CreateFileSymlinks.Size = new Size(144, 23);
+            CreateFileSymlinks.TabIndex = 12;
+            CreateFileSymlinks.Text = "Create File Symlinks";
+            CreateFileSymlinks.UseVisualStyleBackColor = true;
+            CreateFileSymlinks.Click += CreateFileSymlinks_Click;
+            // 
+            // CreateFileHardlinks
+            // 
+            CreateFileHardlinks.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CreateFileHardlinks.Location = new Point(162, 439);
+            CreateFileHardlinks.Name = "CreateFileHardlinks";
+            CreateFileHardlinks.Size = new Size(144, 23);
+            CreateFileHardlinks.TabIndex = 13;
+            CreateFileHardlinks.Text = "Create File Hardlinks";
+            CreateFileHardlinks.UseVisualStyleBackColor = true;
+            CreateFileHardlinks.Click += CreateFileHardlinks_Click;
+            // 
+            // OutputLogToText
+            // 
+            OutputLogToText.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            OutputLogToText.Location = new Point(548, 410);
+            OutputLogToText.Name = "OutputLogToText";
+            OutputLogToText.Size = new Size(144, 23);
+            OutputLogToText.TabIndex = 14;
+            OutputLogToText.Text = "Save LOG to text";
+            OutputLogToText.UseVisualStyleBackColor = true;
+            OutputLogToText.Click += OutputLogToText_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(704, 473);
+            Controls.Add(OutputLogToText);
+            Controls.Add(CreateFileHardlinks);
+            Controls.Add(CreateFileSymlinks);
+            Controls.Add(CreateFolderJucntions);
+            Controls.Add(CreateFoldersSymlinks);
+            Controls.Add(OutputFolders);
+            Controls.Add(SelectOutputs_bt);
+            Controls.Add(LogText);
+            Controls.Add(LOG_label);
+            Controls.Add(InputFiles);
+            Controls.Add(SelectFiles_bt);
+            Controls.Add(InputFolders);
+            Controls.Add(SelectFolders_bt);
+            MinimumSize = new Size(484, 270);
+            Name = "Form1";
+            Text = "MKLink Helper";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button SelectFolders_bt;
+        private OpenFileDialog SelectFiles_fn;
+        private FolderBrowserDialog SelectFolders_fn;
+        private TextBox InputFolders;
+        private Button SelectFiles_bt;
+        private TextBox InputFiles;
+        private Label LOG_label;
+        private TextBox LogText;
+        private Button SelectOutputs_bt;
+        private TextBox OutputFolders;
+        private Button CreateFoldersSymlinks;
+        private Button CreateFolderJucntions;
+        private Button CreateFileSymlinks;
+        private Button CreateFileHardlinks;
+        private Button OutputLogToText;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+using System.Diagnostics;
+using System.Text;
+
+namespace MKLinkHelper_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LogText.ScrollBars = ScrollBars.Both;
+            SelectFolders_fn.Multiselect = true;
+            SelectFiles_fn.Multiselect = true;
+            // Enable drag and drop for IN_Folders TextBox
+            InputFolders.DragEnter += (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            };
+            InputFolders.DragDrop += (s, e) =>
+            { // Drad and drop can't be nulled, so no need to check for null
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    var droppedPaths = ((string[])e.Data.GetData(DataFormats.FileDrop))
+                        .Where(Directory.Exists)
+                        .ToArray();
+                    InputFolders.Text = '"' + string.Join("\";\"", droppedPaths) + '"';
+                }
+            };
+            InputFiles.DragEnter += (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            };
+            InputFiles.DragDrop += (s, e) =>
+            { // Drad and drop can't be nulled, so no need to check for null
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    var droppedPaths = ((string[])e.Data.GetData(DataFormats.FileDrop))
+                        .Where(File.Exists)
+                        .ToArray();
+                    InputFiles.Text = '"' + string.Join("\";\"", droppedPaths) + '"';
+                }
+            };
+            OutputFolders.DragEnter += (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            };
+            OutputFolders.DragDrop += (s, e) =>
+            { // Drad and drop can't be nulled, so no need to check for null
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    var droppedPaths = ((string[])e.Data.GetData(DataFormats.FileDrop))
+                        .Where(Directory.Exists)
+                        .ToArray();
+                    OutputFolders.Text = '"' + string.Join("\";\"", droppedPaths) + '"';
+                }
+            };
+        }
+
+        private void SelectFolders_bt_Click(object sender, EventArgs e)
+        {
+            DialogResult result = SelectFolders_fn.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                InputFolders.Text = '"' + string.Join("\";\"", SelectFolders_fn.SelectedPaths) + '"';
+            }
+        }
+
+        private void SelectFiles_bt_Click(object sender, EventArgs e)
+        {
+            SelectFiles_fn.Title = "Select Files";
+
+            DialogResult result = SelectFiles_fn.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                InputFiles.Text = '"' + string.Join("\";\"", SelectFiles_fn.FileNames) + '"';
+            }
+        }
+
+        private void RunCMDandLog(List<string> list_of_commands_to_run, TextBox LogText)
+        {
+            // Build a single command string; use " & " to run each command sequentially regardless of previous exit codes.
+            // Append "2>&1" so stderr is redirected into stdout and we only need to read one stream.
+            string combinedCommands = string.Join(" & ", list_of_commands_to_run);
+            string arguments = "/c " + combinedCommands + " 2>&1";
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                StandardOutputEncoding = Encoding.UTF8
+            };
+
+            using var proc = new Process { StartInfo = psi };
+            try
+            {
+                proc.Start();
+
+                // Read merged stdout/stderr synchronously on the UI thread.
+                string? line;
+                while ((line = proc.StandardOutput.ReadLine()) != null)
+                {
+                    LogText.AppendText(line + Environment.NewLine);
+                    // Keep UI responsive while remaining single-threaded.
+                    Application.DoEvents();
+                }
+
+                // Ensure the process has exited before returning.
+                proc.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                LogText.AppendText("[EX] " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void CreateFoldersSymlinks_Click(object sender, EventArgs e)
+        {
+            // making list of cmd commands to run
+            var list_of_commands_to_run = new List<string>();
+
+            foreach (string input_folder in InputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (string output_folder in OutputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string trimmed_input_folder = input_folder.Trim().Trim('"');
+                    string trimmed_output_folder = output_folder.Trim().Trim('"');
+                    if (Directory.Exists(trimmed_input_folder) && Directory.Exists(trimmed_output_folder))
+                    {
+                        // Create the junction command
+                        string output_junction = Path.Combine(trimmed_output_folder, Path.GetFileName(trimmed_input_folder));
+                        string cmd = $"mklink /D \"{output_junction}\" \"{trimmed_input_folder}\"";
+                        list_of_commands_to_run.Add(cmd);
+                    }
+                }
+            }
+            RunCMDandLog(list_of_commands_to_run, LogText);
+        }
+
+        private void CreateFolderJucntions_Click(object sender, EventArgs e)
+        {
+            // making list of cmd commands to run
+            var list_of_commands_to_run = new List<string>();
+            foreach (string input_folder in InputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (string output_folder in OutputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string trimmed_input_folder = input_folder.Trim().Trim('"');
+                    string trimmed_output_folder = output_folder.Trim().Trim('"');
+                    if (Directory.Exists(trimmed_input_folder) && Directory.Exists(trimmed_output_folder))
+                    {
+                        // Create the junction command
+                        string output_junction = Path.Combine(trimmed_output_folder, Path.GetFileName(trimmed_input_folder));
+                        string cmd = $"mklink /J \"{output_junction}\" \"{trimmed_input_folder}\"";
+                        list_of_commands_to_run.Add(cmd);
+                    }
+                }
+            }
+            RunCMDandLog(list_of_commands_to_run, LogText);
+        }
+
+        private void CreateFileSymlinks_Click(object sender, EventArgs e)
+        {
+            // making list of cmd commands to run
+            var list_of_commands_to_run = new List<string>();
+            foreach (string input_file in InputFiles.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (string output_folder in OutputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string trimmed_input_file = input_file.Trim().Trim('"');
+                    string trimmed_output_folder = output_folder.Trim().Trim('"');
+                    if (File.Exists(trimmed_input_file) && Directory.Exists(trimmed_output_folder))
+                    {
+                        // Create the symlink command
+                        string output_symlink = Path.Combine(trimmed_output_folder, Path.GetFileName(trimmed_input_file));
+                        string cmd = $"mklink \"{output_symlink}\" \"{trimmed_input_file}\"";
+                        list_of_commands_to_run.Add(cmd);
+                    }
+                }
+            }
+            RunCMDandLog(list_of_commands_to_run, LogText);
+        }
+
+        private void CreateFileHardlinks_Click(object sender, EventArgs e)
+        {
+            // making list of cmd commands to run
+            var list_of_commands_to_run = new List<string>();
+            foreach (string input_file in InputFiles.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (string output_folder in OutputFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string trimmed_input_file = input_file.Trim().Trim('"');
+                    string trimmed_output_folder = output_folder.Trim().Trim('"');
+                    if (File.Exists(trimmed_input_file) && Directory.Exists(trimmed_output_folder))
+                    {
+                        // Create the hardlink command
+                        string output_hardlink = Path.Combine(trimmed_output_folder, Path.GetFileName(trimmed_input_file));
+                        string cmd = $"mklink /H \"{output_hardlink}\" \"{trimmed_input_file}\"";
+                        list_of_commands_to_run.Add(cmd);
+                    }
+                }
+            }
+            RunCMDandLog(list_of_commands_to_run, LogText);
+        }
+
+        private void OutputLogToText_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Title = "Save LOG to Text File",
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                DefaultExt = "txt"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.File.WriteAllText(saveFileDialog.FileName, LogText.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving log: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+			}
+		}
+    }
+}
