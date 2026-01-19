20575f036f45b408af9dc3401019fb408c350395
BuildTools <unconfigured@null.spigotmc.org>
2026-01-19 18:40:16 +0100

    Added Dark Mode using WinForms default implementation, checks for system theme to auto switch on app start

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Win32;
+
 namespace MKLinkHelper_WinForms
 {
     internal static class Program
@@ -8,9 +10,20 @@ namespace MKLinkHelper_WinForms
         [STAThread]
         static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+			// To customize application configuration such as set high DPI settings or default font,
+			// see https://aka.ms/applicationconfiguration.
+
+			// Check Windows theme (light or dark)
+			// this doesn't work for some reason --> Application.SetColorMode(SystemColorMode.System);
+			using var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+			var value = key?.GetValue("AppsUseLightTheme");
+
+            if (value is int intValue && intValue == 0)
+            {
+                Application.SetColorMode(SystemColorMode.Dark);
+            }
+
+			ApplicationConfiguration.Initialize();
             Application.Run(new Form1(args));
         }
     }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MKLinkHelper-WinForms
 
 ![Main Window](https://i.imgur.com/bOWU8zo.png)
+![Dark Mode](https://i.imgur.com/giFxXxk.png)
